272d7742c2c386ff925929c5c1c19cfed3063ace
karl-police <karl-police2001@bluewin.ch>
2015-04-19 23:43:05 +0200

    Create sh_infect.lua

---
+++ headcrab_faction/commands/sh_infect.lua
@@ -0,0 +1,57 @@
+local PLUGIN = PLUGIN;
+
+local COMMAND = Clockwork.command:New("Infect");
+COMMAND.tip = "Infects the peoples, if they are near to you.";
+COMMAND.text = "<string>";
+COMMAND.flags = bit.bor(CMD_DEFAULT, CMD_DEATHCODE, CMD_FALLENOVER);
+COMMAND.arguments = 1;
+
+	 
+
+
+-- Called when the command has been run.
+function COMMAND:OnRun(player, arguments)
+    if (PLUGIN:IsHeadcrabFaction(player:GetFaction())) then
+	    if (string.find(player:GetModel(), "models/Lamarr.mdl"))  then	
+        for k, v in ipairs(player.GetAll()) do
+        if (player:GetPos():Distance(v:GetPos()) <= 3) then
+        v:SetModel("models/Zombie/Classic.mdl")
+		    v:EmitSound("npc/headcrab/headbite.wav", 60);
+            end
+        end
+	else
+		Clockwork.player:Notify("You're not an Headcrab!")	
+    end
+end	
+    elseif (PLUGIN:IsHeadcrabFaction(player:GetFaction())) then
+        if (string.find(player:GetModel(), "models/headcrab.mdl"))  then	
+		for k, v in ipairs(player.GetAll()) do
+		if (player:GetPos():Distance(v:GetPos()) <= 3) then
+		v:SetModel("models/Zombie/Fast.mdl")
+		    v:EmitSound("npc/headcrab_fast/headbite.wav", 60);
+            end
+        end
+	else
+		Clockwork.player:Notify("You're not an Fast Headcrab!")
+    end
+end	
+	elseif (PLUGIN:IsHeadcrabFaction(player:GetFaction())) then
+        if (string.find(player:GetModel(), "models/headcrabblack.mdl"))  then	
+		for k, v in ipairs(player.GetAll()) do
+		if (player:GetPos():Distance(v:GetPos()) <= 3) then
+		v:SetModel("models/Zombie/Poison.mdl")
+		    v:EmitSound("npc/headcrab_poison/ph_poisonbite1.wav", 60);
+            end;
+        end;
+	else
+		Clockwork.player:Notify("You're not an Poison Headcrab!")
+    end;
+end;
+			end;
+		end;
+	else
+		Clockwork.player:Notify("You're not an Headcrab!")
+	end;
+end;
+
+COMMAND:Register();

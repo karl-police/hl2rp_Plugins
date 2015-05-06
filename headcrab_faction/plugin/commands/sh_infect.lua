aea72769bb642ca306e7cca98c2c38b89831ab71
karl-police <karl-police2001@bluewin.ch>
2015-05-06 13:05:38 +0200

    Update sh_infect.lua

--- headcrab_faction/plugin/commands/sh_infect.lua
+++ headcrab_faction/plugin/commands/sh_infect.lua
@@ -2,17 +2,15 @@ local PLUGIN = PLUGIN;
 
 local COMMAND = Clockwork.command:New("Infect");
 COMMAND.tip = "Infects the peoples, if they are near to you.";
-COMMAND.text = "<string>";
 COMMAND.flags = bit.bor(CMD_DEFAULT, CMD_DEATHCODE, CMD_FALLENOVER);
-COMMAND.arguments = 1;
 
 	 
 
 
 -- Called when the command has been run.
-function COMMAND:OnRun(player, arguments)
+function COMMAND:OnRun(player)
     if(PLUGIN:IsHeadcrabFaction(player:GetFaction())) then
-	    if (string.find(player:GetModel(), "models/Lamarr.mdl"))  then	
+	    if (string.find(player:GetModel(), "models/headcrabclassic.mdl"))  then	
         for k, v in ipairs(player.GetAll()) do
         if (player:GetPos():Distance(v:GetPos()) <= 3) then
         v:SetModel("models/Zombie/Classic.mdl")
@@ -22,7 +20,7 @@ function COMMAND:OnRun(player, arguments)
 	else
 		Clockwork.player:Notify("You're not an Headcrab!")	
     end
-end	
+	
     elseif(PLUGIN:IsHeadcrabFaction(player:GetFaction())) then
         if (string.find(player:GetModel(), "models/headcrab.mdl"))  then	
 		for k, v in ipairs(player.GetAll()) do
@@ -45,6 +43,8 @@ end
         end;
 	else
 		Clockwork.player:Notify("You're not an Poison Headcrab!")
+    end;
+	
     end;
 end;
 
